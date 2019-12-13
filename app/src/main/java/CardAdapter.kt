2f2d51588cb08c241298a064a9d85477a6add589
AnjayGoel <angoel123@gmail.com>
2019-12-13 12:16:58 +0530

    Moved to Room DB form JSON.

--- app/src/main/java/CardAdapter.kt
+++ app/src/main/java/CardAdapter.kt
@@ -1,6 +1,7 @@
 package com.anjay.notify
 
 import android.content.Context
+import android.graphics.Color
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -8,18 +9,21 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.anjay.notify.com.anjay.notify.SliderImageAdapter
+import com.smarteist.autoimageslider.IndicatorAnimations
+import com.smarteist.autoimageslider.SliderAnimations
+import com.smarteist.autoimageslider.SliderView
 
-class CardAdapter(val items: MutableList<CardModel>, val context: Context) :
+class CardAdapter(val items: List<Card>, val context: Context) :
     RecyclerView.Adapter<ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         var card = items[position]
-        var parent: ViewGroup = holder.h.parent as ViewGroup
-        var text = card.summary.replace("\\n", "\n")
-
+        var parent: ViewGroup? = holder.h.parent as? ViewGroup
+        var text = card.body.replace("\\n", "\n")
         //heading
         if (card.head == "") {
-            (parent).removeView(holder.h)                                                          //remove if no heading
+            (parent)?.removeView(holder.h)                                                          //remove if no heading
         } else holder.h.text = card.head
 
         //body
@@ -32,41 +36,58 @@ class CardAdapter(val items: MutableList<CardModel>, val context: Context) :
             holder.summary.setOnClickListener { v ->
                 //view more if text to long
                 var tb = v as TextView
-                tb.text = card.summary
+                tb.text = card.body
+
+                holder.iv_container.removeView(holder.iv_one)
+                holder.iv_container.removeView(holder.iv_two)
+                holder.iv_container.removeView(holder.iv_three_or_plus)
+                holder.iv_container.addView(holder.sv)
+
+
             }
 
         } else {
-            holder.summary.text = card.summary
+            holder.summary.text = card.body
         }
 
+        holder.sv.sliderAdapter = SliderImageAdapter(context)
+        holder.sv.setIndicatorAnimation(IndicatorAnimations.WORM) //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        holder.sv.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION)
+        holder.sv.autoCycleDirection = SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH
+        holder.sv.indicatorSelectedColor = Color.WHITE
+        holder.sv.indicatorUnselectedColor = Color.GRAY
+        holder.sv.scrollTimeInSec = 4 //set scroll delay in seconds :
+        holder.sv.startAutoCycle()
+
         //date
-        holder.posttime.text = timeFromString(card.time)
-        Log.d("sb", "" + card.images.size)
+        holder.posttime.text = timeFromString(card.timestamp)
+        Log.d("sb", "" + card.images!!.size)
+
 
         //images
-        when (card.images.size) {
+        holder.iv_container.removeView(holder.sv)
+        when (card.images!!.size) {
             0 -> {
-                parent.removeView(holder.iv_one)
-                parent.removeView(holder.iv_two)
-                parent.removeView(holder.iv_three_or_plus)
+                holder.iv_container.removeView(holder.iv_one)
+                holder.iv_container.removeView(holder.iv_two)
+                holder.iv_container.removeView(holder.iv_three_or_plus)
             }
             1 -> {
-                parent.removeView(holder.iv_two)
-                parent.removeView(holder.iv_three_or_plus)
+                holder.iv_container.removeView(holder.iv_two)
+                holder.iv_container.removeView(holder.iv_three_or_plus)
             }
             2 -> {
-                parent.removeView(holder.iv_one)
-                parent.removeView(holder.iv_three_or_plus)
+                holder.iv_container.removeView(holder.iv_one)
+                holder.iv_container.removeView(holder.iv_three_or_plus)
             }
             3 -> {
-                parent.removeView(holder.iv_two)
-                parent.removeView(holder.iv_one)
+                holder.iv_container.removeView(holder.iv_two)
+                holder.iv_container.removeView(holder.iv_one)
             }
             else -> {
-                parent.removeView(holder.iv_two)
-                parent.removeView(holder.iv_one)
+                holder.iv_container.removeView(holder.iv_two)
+                holder.iv_container.removeView(holder.iv_one)
             }
-
 
         }
 
@@ -91,7 +112,9 @@ class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     var h: TextView = view.findViewById(R.id.h)
     var posttime: TextView = view.findViewById(R.id.posttime)
     var summary: TextView = view.findViewById(R.id.summary)
+    var iv_container: ViewGroup = view.findViewById(R.id.iv_container)
     var iv_one: ViewGroup = view.findViewById(R.id.image_one)
-    var iv_two: ViewGroup = view.findViewById(R.id.image_tow)
+    var iv_two: ViewGroup = view.findViewById(R.id.image_two)
     var iv_three_or_plus: ViewGroup = view.findViewById(R.id.image_three_or_plus)
+    var sv: SliderView = view.findViewById(R.id.imageSlider)
 }
